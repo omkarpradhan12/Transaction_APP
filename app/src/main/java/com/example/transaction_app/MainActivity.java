9b61255a4328b30e539492126411757ff7d9eaa0
omkarpradhan12 <42708220+omkarpradhan12@users.noreply.github.com>
2022-09-18 18:29:21 +0530

    Said hello

--- app/src/main/java/com/example/transaction_app/MainActivity.java
+++ app/src/main/java/com/example/transaction_app/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String greet = "Hello Panda";
+        String greet = "Hello Harshi";
     }
 }
